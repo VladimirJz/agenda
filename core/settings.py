466f81dbd8c9756660174caaa1347707611fdcb9
Vladimir <vladimir@pop-os.localdomain>
2022-02-24 08:50:45 -0600

    templatetag test2

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'floppyforms',
     'apps.home',  # Enable the inner home (home)
     'apps.company',
